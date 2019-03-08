926af8c131eab30276b6d16276a4291874c107f0
anthonyjamesgoddard <antg3254@gmail.com>
2019-03-08 20:03:28 +0000

    finshed ex1

--- threadEx1.cpp
+++ threadEx1.cpp
@@ -1,14 +1,25 @@
 #include <boost/timer/timer.hpp>
+#include <boost/thread.hpp>
+#include <boost/ref.hpp>
 #include <iostream>
 #include <cstdint>
+
+void count(int start, int end, std::uint64_t& tot)
+{
+	for(int i = start; i < end; i++)
+		tot += i;
+}
 
 int main()
 {
     boost::timer::cpu_timer timer;
 
-    std::uint64_t total = 0;
-    for (int i = 0; i < 1'000'000'000; ++i)
-        total += i;
+    std::uint64_t tot1 = 0, tot2 = 0;
+	boost::thread thread_1(count, 0, 500, boost::ref(tot1));
+	thread_1.join();
+	boost::thread thread_2(count, 501, 1000, boost::ref(tot2));
+	thread_2.join();
+	auto total = tot1 + tot2;
 
     std::cout << timer.format();
     std::cout << total << '\n';
